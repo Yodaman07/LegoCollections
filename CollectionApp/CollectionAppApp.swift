a01baa0f4800fee6c8f83c2139218be7d0b272f7
Yodaman07 <ayaan.irshad@gmail.com>
2023-01-19 10:16:28 -0800

    Initial Commit

---
+++ CollectionApp/CollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionAppApp.swift
+//  CollectionApp
+//
+//  Created by Ayaan Irshad on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
